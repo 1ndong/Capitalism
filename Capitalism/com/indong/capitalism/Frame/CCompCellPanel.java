47750b679fa175d59ceeb21efb00e51e9b719e8d
1ndong <ssaulle@naver.com>
2020-12-26 22:09:22 +0900

    버튼리스너

--- Capitalism/com/indong/capitalism/Frame/CCompCellPanel.java
+++ Capitalism/com/indong/capitalism/Frame/CCompCellPanel.java
@@ -1,31 +1,36 @@
 package com.indong.capitalism.Frame;
 
-import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class CCompCellPanel extends JPanel {
-	 private static final long serialVersionUID = 1L;
-	    private JLabel name = new JLabel();
-	    private JLabel balance = new JLabel();
-	    private JButton detailButton = new JButton("detail");
+	private static final long serialVersionUID = 1L;
+	private JLabel name = new JLabel();
+	private JLabel balance = new JLabel();
+	private JButton detailButton = new JButton("detail");
 
-	   public CCompCellPanel(String name , int balance) {
-	        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-	        this.name.setText(name);
-	        this.balance.setText(String.valueOf(balance));
-	        add(this.name);
-	        add(this.balance);
-	        add(detailButton);
-	    }
+	public CCompCellPanel(String name, int balance) {
+		setLayout(new GridLayout(1,3));
+		this.name.setText("name = "+name);
+		this.balance.setText("balance = "+balance);
+		detailButton.addActionListener(new CDetailButtonClickListener());
+		add(this.name);
+		add(this.balance);
+		add(detailButton);
+	}
 
-	    public void setComp(CTableComp comp) {
-	        name.setText(comp.name);
-	        balance.setText(String.valueOf(comp.balance));
-	    }
+	public void setComp(CTableComp comp) {
+		name.setText("name = "+comp.name);
+		balance.setText("balance = "+comp.balance);
+	}
 
-	    public CTableComp getComp() {
-	        return new CTableComp(name.getText(), Integer.parseInt(balance.getText()));
-	    }
+	public CTableComp getComp() {
+		String balancetext = balance.getText();
+		int lastspace = balancetext.lastIndexOf(" ");
+		int onlybalance = Integer.parseInt(balancetext.substring(lastspace+1));
+		return new CTableComp(name.getText(), onlybalance);
+	}
 }
---
+++ Capitalism/com/indong/capitalism/Frame/CDetailButtonClickListener.java
@@ -0,0 +1,15 @@
+package com.indong.capitalism.Frame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class CDetailButtonClickListener implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "not yet");
+	}
+}
